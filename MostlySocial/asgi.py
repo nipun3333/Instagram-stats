31be4a16cf0bbae78aa9e03de48d71644d3cf8bb
nipun3333 <70288062+nipun3333@users.noreply.github.com>
2021-05-20 23:49:03 +0530

    Add files via upload

---
+++ MostlySocial/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for MostlySocial project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MostlySocial.settings')
+
+application = get_asgi_application()
